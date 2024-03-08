b7c89671f81c1d8193c288bfe83636f1240d18cb
Alex <user@user.com>
2024-03-09 02:25:50 +0300

    4 task: night edits. Sorting users 	modified:   4/index.html 	modified:   4/script.js 	modified:   4/style.css

--- 4/script.js
+++ 4/script.js
@@ -10,40 +10,30 @@
     //слушатель на кнопке для подгрузки новых пользователей 
     document.querySelector('.all-users__load-button').addEventListener('click', () => getUsers());
 
-    //замыкание со счетчиком страниц page
-    //и запуск асинхронной функции getUsersData для получения данных с сервера
-    //сортировка и создание карточек пользователей функцией createCard
-    //отображение количества загруженных пользователей
-    //и отключение отображения кнопки подзагрузки, если загружены все пользователи
     function showUsers(n = 0) {
         const loadButton = document.querySelector('.all-users__load-button');
-        let page = n;
+        let page = n; //замыкание со счетчиком страниц page,
+        let arrUsers = []; //в этом массиве храню всех пользователей
 
         return async function () {
             page++;
-            const data = await getUsersData(page);
+            const data = await getUsersData(page); //запуск асинхронной функции getUsersData для получения данных с сервера
 
             if (data) {
-                const divPageUsers = document.createElement('div');
-                const h3PageNumber = document.createElement('h3');
-                const hr = document.createElement('hr');
-                divPageUsers.classList.add(`all-users__users${page}`);
-                divPageUsers.classList.add('users');
+                arrUsers.push(...data?.data); //добавляю полученных пользователей в массив arrUsers
 
-                h3PageNumber.classList.add('pages__page-number');
-                h3PageNumber.textContent = `Страница ${page}`;
-                document.querySelector('.all-users__pages').append(h3PageNumber);
+                arrUsers.sort((a, b) => a.first_name.localeCompare(b.first_name)); //сортирую массив
 
-                const sortedUsers = data?.data.sort((a, b) => a.first_name.localeCompare(b.first_name));
-
-                document.querySelector('.all-users__pages').append(divPageUsers);
-                sortedUsers.forEach(user => createCard(user, page));
-                document.querySelector('.all-users__pages').append(hr);
+                document.querySelector('.all-users__users').innerHTML = ''; //очищаю div от результатов предыдущего рендера
+                arrUsers.forEach(user => createCard(user, page));//создание карточек пользователей
 
                 const loadedUsers = document.querySelectorAll('.card').length;
 
+                //отображение количества загруженных пользователей
                 document.querySelector('.all-users__total').textContent = `
         Загружено пользователей: ${loadedUsers} из ${data.total}`;
+
+                //отключение отображения кнопки подзагрузки, если загружены все пользователи
                 if (loadedUsers === data.total) loadButton.style.display = 'none';
             }
         };
@@ -55,8 +45,7 @@
             const response = await fetch(`https://reqres.in/api/users?page=${page}`);
             if (!response.ok) throw new Error('Ошибка при выполнении запроса');
 
-            const data = await response.json();
-            return data;
+            return await response.json();
         } catch (error) {
             console.error('Произошла ошибка:', error.message);
             document.querySelector('.all-users__error').append(`Произошла ошибка: ${error.message} `);
@@ -64,7 +53,7 @@
     }
 
     //функция для создания карточки пользователя по темплейту в HTML
-    function createCard(user, page) {
+    function createCard(user) {
         const template = document.querySelector('#template-character'),
             fragment = template.content.cloneNode(true);
 
@@ -73,6 +62,6 @@
         fragment.querySelector('.card__user-email .email').textContent = user.email;
         fragment.querySelector('.card__user-email .email').href = `mailto:${user.email}`;
 
-        document.querySelector(`.all-users__users${page}`).append(fragment);
+        document.querySelector('.all-users__users').append(fragment);
     }
 })();
